577e8106f0519c1db50429c6024d37031265d11f
Curtis Burkhalter <curtis.burkhalter@hp.com>
2025-11-17 10:47:45 -0700

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -5,7 +5,7 @@ mkdir -p models
 
 # Download models from your S3 bucket
 echo "Downloading vision language model..."
-aws s3 cp s3://finetuning-demo-models/vit-gpt2-image-captioning/ models/vit-gpt2-image-captioning/ --recursive
+aws s3 cp https://finetuning-demo-models.s3.us-west-2.amazonaws.com/vit-gpt2-image-captioning/ models/vit-gpt2-image-captioning/ --recursive
 
 
 echo "Models downloaded successfully!"
